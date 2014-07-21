a75074acdbfddf263304847e6a9ab7ac94f5f611
Alex Parkinson <okalex@gmail.com>
2014-07-21 09:24:33 -0700

    Unique-ify and sort requested countries list

--- app.rb
+++ app.rb
@@ -1,24 +1,33 @@
 require 'sinatra'
 require 'sinatra/param'
+require 'yaml'
 
 helpers Sinatra::Param
+
+country_codes = YAML.load_file('data/country_codes.yaml')
 
 get '/' do
   erb :index, :locals => {:src => "/map?countries=#{params[:countries]}"}
 end
 
 get '/map' do
-  param :countries,  Array, default: []
+  param :countries, Array, default: []
 
   # This should be an alphabetized set
-  countries = params[:countries] || []
-  if countries.empty?
+  requested_countries = params[:countries] || []
+  if requested_countries.empty?
     filter = ''
   else
-    filter = "-where \"ADM0_A3 IN ('#{countries.join("','")}')\""
+    requested_countries.map!(&:upcase).uniq!
+    requested_countries.select! do |country|
+      country_codes.has_key?(country)
+    end
+    requested_countries.sort!
+    puts requested_countries
+    filter = "-where \"ADM0_A3 IN ('#{requested_countries.join("','")}')\""
   end
 
-  cache_file = "/cache/world_#{countries.join('_')}.json"
+  cache_file = "/cache/world_#{requested_countries.join('_')}.json"
   unless File.exist?("public#{cache_file}")
     `rm tmp/land.json`
     ogr_cmd = "ogr2ogr -f GeoJSON #{filter} tmp/land.json data/countries.shp"
